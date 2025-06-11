c9681dc170d7c6cc3ea6e5a31ae211975fe6e8b9
Desuwaa <airekpita@gmail.com>
2025-06-11 20:48:15 +0100

    compared user answer to sytstem answer

--- quiz.js
+++ quiz.js
@@ -1,5 +1,13 @@
 'use strict';
+
 function checkAnswer() {
   const correctAnswer = '4';
   const userAnswer = document.querySelector('[name="quiz"]').value;
+
+  if (correctAnswer === userAnswer) {
+    document.getElementById('feedback').textContent = 'Correct! Well done.';
+  } else {
+    document.getElementById('feedback').textContent =
+      "That's incorrect. Try again!";
+  }
 }
